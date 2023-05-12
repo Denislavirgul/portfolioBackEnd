76333152f3cbd02df4b1ee0258c92ebb9fdacdcf
denislavirgul <lavirgul@gmail.com>
2023-05-12 11:26:57 -0300

    /bara sobrante

--- src/main/java/com/portfolio2/newproyecto/Controller/CHys.java
+++ src/main/java/com/portfolio2/newproyecto/Controller/CHys.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://denisefrontend.web.app/","http://localhost:4200"})
+@CrossOrigin(origins = {"https://denisefrontend.web.app","http://localhost:4200"})
 @RequestMapping("/skill")
 public class CHys {
     @Autowired
