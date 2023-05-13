ed2396598bd8bbbe11ac48316641a29cd829ec3b
denislavirgul <lavirgul@gmail.com>
2023-05-13 19:22:53 -0300

    cambio url

--- src/main/java/com/portfolio2/newproyecto/Controller/CEducacion.java
+++ src/main/java/com/portfolio2/newproyecto/Controller/CEducacion.java
@@ -26,7 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://denisefrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://denisefrontend.web.app")
+//@CrossOrigin(origins = {"https://denisefrontend.web.app","http://localhost:4200"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/portfolio2/newproyecto/Controller/CExperiencia.java
+++ src/main/java/com/portfolio2/newproyecto/Controller/CExperiencia.java
@@ -26,7 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = {"https://denisefrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://denisefrontend.web.app")
+//@CrossOrigin(origins = {"https://denisefrontend.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
